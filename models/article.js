e8b6be84d24d17288957f7a0eeed80991656d53f
Anthony Baker <abakerp@gmail.com>
2016-12-29 12:36:24 +0000

    adding Article model and initial testing routes

---
+++ models/article.js
@@ -0,0 +1,15 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our beer schema
+var ArticleSchema   = new mongoose.Schema({
+  title: String,
+  summary: String, 
+  publishDate: Date,
+  publicationName: String,
+  articleUrl: String,
+  imageUrl: String
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Article', ArticleSchema);
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // Get the packages we need
 var express = require('express');
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-require('dotenv').config()
-var Schema = mongoose.Schema;
+var bodyParser = require('body-parser');
 
+// load modules required
+var Article = require('./models/article');
+
+// load environment variables
+require('dotenv').config()
+
+// set db connection settings
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
@@ -14,39 +19,21 @@ console.log(mongodbURI);
 // Connect to the beerlocker MongoDB
 mongoose.connect(mongodbURI, options);
 
-var conn = mongoose.connection;             
+// var conn = mongoose.connection;             
  
-conn.on('error', console.error.bind(console, 'connection error:'));  
- 
-var User = mongoose.model('User', new Schema({
-    name: String,
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    admin: Boolean,
-    location: String,
-    created_at: Date,
-    updated_at: Date
-}));
+// conn.on('error', console.error.bind(console, 'connection error:'));  
 
+// conn.once('open', function() {
+//   // Wait for the database connection to establish, then start the app.  
 
-var new_user = new User({
-    name: 'test2',
-    username: 'test2',
-    password: 'test2'
-});
+//   console.log('started')
+//     new_user.save(function(err) {
+//         if (err) throw err;
+//         console.log('User created!');
+//     });
 
-
-conn.once('open', function() {
-  // Wait for the database connection to establish, then start the app.  
-
-  console.log('started')
-    new_user.save(function(err) {
-        if (err) throw err;
-        console.log('User created!');
-    });
-
-  console.log("connected to the db...");                       
-});
+//   console.log("connected to the db...");                       
+// });
 
 // Create our Express application
 var app = express();
@@ -69,8 +56,45 @@ router.get('/', function(req, res) {
   res.json({ message: 'No work to show' });
 });
 
+// Create a new route with the prefix /beers
+var articlesRoute = router.route('/articles');
+
+// Create endpoint /api/articles for POSTS
+articlesRoute.post(function(req, res) {
+  // Create a new instance of the Article model
+  var article = new Article();
+
+  // Set the article properties that came from the POST data
+  article.title = req.body.title;
+  article.summary = req.body.summary;
+  article.publishDate = req.body.publishDate;
+  article.publicationName = req.body.publicationName;
+  article.articleUrl = req.body.articleUrl;
+  article.imageUrl = req.body.imageUrl;
+
+  // Save the article and check for errors
+  article.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Article added to the portfolio!', data: article });
+  });
+});
+
+// Create endpoint /api/articles for GET
+articlesRoute.get(function(req, res) {
+  // Use the Article model to find all beer
+  Article.find(function(err, articles) {
+    if (err)
+      res.send(err);
+
+    res.json(articles);
+  });
+});
+
 // Register all our routes with /api
 app.use('/api', router);
+
 
 // Start the server
 app.listen(port);
