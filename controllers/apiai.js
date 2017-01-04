9afb76cdfbcbb25609afd60867deaf891c5cb766
Anthony Baker <abakerp@gmail.com>
2017-01-04 16:24:46 +0000

    adding apiai facebook and slack template for get articles

--- controllers/apiai.js
+++ controllers/apiai.js
@@ -1,3 +1,6 @@
+// Load required packages
+var Article = require('../models/article');
+
 // Create endpoint /api/articles for POSTS
 exports.postRequest = function(req, res) {
 
@@ -29,17 +32,84 @@ exports.postRequest = function(req, res) {
 // Get Articles funtion for Api.ai agent
 function getArticles (res) {
 
-  // fullfillment logic
-  resTemplate = {
-      "speech": "here are some articles",
-      "displayText": "here are some articles",
-      "source": "Portfolio Service" 
-  }    
+  // Use the Article model to find all articles
+  Article.find(function(err, articles) {
+    if (err)
+      res.send(err);
 
-  console.log('Response body: ' + JSON.stringify(resTemplate));
+    // Generate the Facebook template structure for API.ai agent
+    var count = 0;
+    var fbArticles = [];
+    fbArticle = "";
 
-  res.setHeader('content-type', 'application/json');
-  res.status(200).json(resTemplate);
+    articles.forEach(function(article) {
+
+      fbArticle = {
+        "title": article.title,
+        "image_url": article.imageUrl,
+        "item_url": article.articleUrl,
+        "subtitle": article.publicationName,
+        "buttons": [
+          {
+            "type": "web_url",
+            "url": article.articleUrl,
+            "title": "Open Article"
+          },
+          {
+            "type": "postback",
+            "title": "Select",
+            "payload": article._id
+          }
+        ]
+      }
+
+      fbArticles.push(fbArticle);  
+
+    });
+
+    facebook = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements":
+            /* Here goes the list of contacts */
+            fbArticles
+        }
+      }
+    }
+      
+    slack = {
+      "text": "got the message, here's an answer from the webhook",
+      "attachments": [
+          {
+              "fallback": "John Smith - R/GA Employee - http://res.cloudinary.com/abakerp/image/upload/v1480872817/Backgrounds_The_man_in_black_tie_zq8mam.jpg",
+              "title": "John Smith",
+              "title_link": "https://www.rga.com",
+              "text": "R/GA Employee",
+              "image_url": "http://res.cloudinary.com/abakerp/image/upload/v1480872817/Backgrounds_The_man_in_black_tie_zq8mam.jpg",
+              "color": "#764FA5"
+          }
+      ]
+    }
+
+    resTemplate = {
+      "speech": "got the message, here's an answer from the webhook",
+      "displayText": "got the message, here's an answer from the webhook",
+      "source": "Contacts-Manager-Webhook",
+      "data": {
+          facebook,
+          slack
+        
+      }
+    }
+
+    console.log('Response body: ' + JSON.stringify(resTemplate));
+
+    res.setHeader('content-type', 'application/json');
+    res.status(200).json(resTemplate);
+
+  });
   
 }
 
